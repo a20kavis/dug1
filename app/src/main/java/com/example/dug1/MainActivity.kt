41980bcf5e6fbac02d0f6a35180cb0e691d49b84
BuildTools <unconfigured@null.spigotmc.org>
2021-03-31 12:37:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dug1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dug1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//commit message
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
